8da8249c69cf493271a56646a3d86f4c82d7c7ea
Irina Rakova <lis.ovskaya1119@gmail.com>
2024-04-08 14:47:48 +0300

    Initial commit

---
+++ src/main/java/ru/irya_lis/springcourse/controllers/PeopleControllers.java
@@ -0,0 +1,63 @@
+package ru.irya_lis.springcourse.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.irya_lis.springcourse.dao.PersonDao;
+import ru.irya_lis.springcourse.models.Person;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleControllers {
+
+    private final PersonDao personDao;
+
+    public PeopleControllers(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("people", personDao.index());
+        return "people/index";
+    }
+
+    @GetMapping("{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDao.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id ) {
+        model.addAttribute("person", personDao.show(id));
+        return "people/edit";
+
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDao.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        personDao.delete(id);
+        return "redirect:/people";
+    }
+
+}
